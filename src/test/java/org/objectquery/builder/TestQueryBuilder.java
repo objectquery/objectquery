5df79a1a938b9ca323a6b7758cfc4c5d1a33c3d2
tglman <tglman@tglman.com>
2012-08-27 10:33:19 +0200

    clear test code.

--- src/test/java/org/objectquery/builder/TestQueryBuilder.java
+++ src/test/java/org/objectquery/builder/TestQueryBuilder.java
@@ -34,12 +34,12 @@ public class TestQueryBuilder extends AbstractInternalQueryBuilder {
 	}
 
 	private void stringfyCondition(ConditionItem cond, StringBuilder sb) {
-		buildPath(((ConditionItem) cond).getItem(), sb);
-		sb.append(" ").append(((ConditionItem) cond).getType()).append(" ");
-		if (((ConditionItem) cond).getValue() instanceof PathItem)
-			buildPath((PathItem) ((ConditionItem) cond).getValue(), sb);
+		buildPath(cond.getItem(), sb);
+		sb.append(" ").append(cond.getType()).append(" ");
+		if (cond.getValue() instanceof PathItem)
+			buildPath((PathItem) cond.getValue(), sb);
 		else
-			sb.append(((ConditionItem) cond).getValue());
+			sb.append(cond.getValue());
 	}
 
 	public void build() {
