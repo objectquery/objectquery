d38c16eecfe4eabbcb4ad1e4e3b4866eb4560411
tglman <tglman@tglman.com>
2012-11-17 18:06:34 +0000

    updated performance test

--- src/test/java/org/objectquery/generic/TestRequiredSpeed.java
+++ src/test/java/org/objectquery/generic/TestRequiredSpeed.java
@@ -2,7 +2,6 @@ package org.objectquery.generic;
 
 import org.junit.Assert;
 import org.junit.Test;
-import org.objectquery.ObjectQuery;
 import org.objectquery.generic.domain.Person;
 
 public class TestRequiredSpeed {
@@ -10,23 +9,37 @@ public class TestRequiredSpeed {
 	@Test
 	public void testSpeed() {
 		long time = System.currentTimeMillis();
-		for (int i = 0; i < 400000; i++) {
-			buildQuery();
+		long ptime = System.currentTimeMillis();
+
+		int size = 100000;
+		int perc = size / 100;
+		MockQueryBuilder builder = new MockQueryBuilder();
+		GenericObjectQuery<Person> query = new GenericObjectQuery<Person>(builder, Person.class);
+
+		for (int i = 0; i < size; i++) {
+			if (i % perc == 0) {
+				System.out.println((i / perc) + " " + ((System.currentTimeMillis() - ptime)));
+				ptime = System.currentTimeMillis();
+			}
+			buildQuery(query, builder);
 		}
 		System.out.println(((System.currentTimeMillis() - time)));
+		System.out.println(Runtime.getRuntime().totalMemory() / 1024 / 1024 + "Mb");
 		Assert.assertTrue(((System.currentTimeMillis() - time) / 1000) < 3600);
 	}
 
-	public void buildQuery() {
-		TestQueryBuilder builder = new TestQueryBuilder();
-		ObjectQuery<Person> query = new GenericObjectQuery<Person>(builder, Person.class);
+	public void buildQuery(GenericObjectQuery<Person> query, MockQueryBuilder builder) {
 		Person toSearch = query.target();
 		query.eq(toSearch.getHome().getAddress(), toSearch.getDog().getHome().getAddress());
 		query.eq(toSearch.getDud().getHome(), toSearch.getDog().getHome());
 		query.eq(toSearch.getMum().getName(), toSearch.getDog().getOwner().getName());
 		query.order(toSearch.getName());
-
 		builder.build();
+		Assert.assertEquals("size of result conditions", 3, builder.getConditionsString().size());
+		Assert.assertEquals("first condition", "home.address EQUALS dog.home.address", builder.getConditionsString().get(0));
+		Assert.assertEquals("second condition", "dud.home EQUALS dog.home", builder.getConditionsString().get(1));
+		Assert.assertEquals("thirdth condition", "mum.name EQUALS dog.owner.name", builder.getConditionsString().get(2));
+		query.clear();
 	}
 
 }
