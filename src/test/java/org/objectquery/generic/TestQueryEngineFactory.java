942cc9aafeb01e8cd4df9fbbdf170a4bf7bb4d15
tglman <tglman@tglman.com>
2013-12-14 00:27:58 +0000

    fix test case collision.

--- src/test/java/org/objectquery/generic/TestQueryEngineFactory.java
+++ src/test/java/org/objectquery/generic/TestQueryEngineFactory.java
@@ -1,5 +1,6 @@
 package org.objectquery.generic;
 
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Test;
 import org.objectquery.QueryEngine;
@@ -9,19 +10,26 @@ public class TestQueryEngineFactory {
 	@Test
 	public void testCreateFromDiscover() {
 		QueryEngine<Object> q = QueryEngine.defaultInstance();
+		System.out.println(q);
 		Assert.assertTrue(q instanceof TestQueryEngine);
 	}
-	
+
 	@Test
 	public void testCreateFromDiscoverBySession() {
 		QueryEngine<Object> q = QueryEngine.instance(Object.class);
 		Assert.assertTrue(q instanceof TestQueryEngine);
 	}
-	
+
 	@Test
 	public void testCreateFromSys() {
 		System.setProperty("org.objectquery.QueryEngine", "org.objectquery.generic.SysTestQueryEngine");
 		QueryEngine<Object> q = QueryEngine.defaultInstance();
 		Assert.assertTrue(q instanceof SysTestQueryEngine);
 	}
+
+	@After
+	public void after() {
+		System.clearProperty("org.objectquery.QueryEngine");
+	}
+
 }
